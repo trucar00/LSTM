1370e2149994e07d62a4f43d02225ab969a2bc08
Truls Carlson <trulsc@ntnu.no>
2026-06-12 13:29:56 +0200

    concat all gear new rule

--- features_2_conf_big.py
+++ features_2_conf_big.py
@@ -31,7 +31,7 @@ def angle_wrap(a):
 # ---------------------------
 
 FEATURES = ["cog_sin", "cog_cos", "speed_calc_ms", "accel", "ra_accel", "jerk", "ra_jerk", "dcog", "ra_dcog", "log_dist", "log_dt"]
-GEAR = ["Trål", "Not", "Krokredskap", "Snurrevad", "Garn", "Traps", "Bur og ruser"]
+GEAR = ["Trål", "Not", "Krokredskap", "Snurrevad", "Garn", "Traps"]
 
 
 def column_fixing(df):
